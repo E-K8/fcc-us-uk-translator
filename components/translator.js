b1983b8e5b698f08b8246876f5d7f5b3ba314819
E-K8 <ekatplat@gmail.com>
2024-08-20 21:56:51 +0100

    add translateToAmerican function

--- components/translator.js
+++ components/translator.js
@@ -9,12 +9,22 @@ class Translator {
   translateToBritish(text) {
     let translatedText = text;
 
-    // Loop through the americanOnly dictionary and replace words
+    // Loop through the american-only dictionary and replace words
     for (const [americanWord, britishWord] of Object.entries(americanOnly)) {
       const regex = new RegExp(`\\b${americanWord}\\b`, 'gi'); // Match whole word
-      //   console.log(`Replacing ${americanWord} with ${britishWord}`);
 
       translatedText = translatedText.replace(regex, `${britishWord}`);
+    }
+    return translatedText;
+  }
+  translateToAmerican(text) {
+    let translatedText = text;
+
+    // Loop through the british-only dictionary and replace words
+    for (const [britishWord, americanWord] of Object.entries(britishOnly)) {
+      const regex = new RegExp(`\\b${britishWord}\\b`, 'gi'); // Match whole word
+
+      translatedText = translatedText.replace(regex, `${americanWord}`);
     }
     return translatedText;
   }
--- test.js
+++ test.js
@@ -3,7 +3,10 @@
 const Translator = require('./components/translator');
 const translator = new Translator();
 
-const text = 'I need vacay and some fries';
-const translated = translator.translateToBritish(text);
+// const text = 'I need vacay and some fries';
+const text = 'There is a car park at the back';
+// const translated = translator.translateToBritish(text);
+const translated = translator.translateToAmerican(text);
 
+// console.log(translated); // Should output: "I need hols and some chips."
 console.log(translated); // Should output: "I need hols and some chips."
