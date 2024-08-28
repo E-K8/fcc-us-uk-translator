f16050194e05d60c2dd1dddc43925f9da7f1bcb9
E-K8 <ekatplat@gmail.com>
2024-08-28 20:28:42 +0100

    api tests

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,41 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route('/api/translate').post((req, res) => {
+    const { text, locale } = req.body;
+
+    if (!text || !locale) {
+      return res.json({
+        error: 'Required field(s) missing',
+      });
+    }
+
+    if (text === '') {
+      return res.json({
+        error: 'No text to translate',
+      });
+    }
+
+    let translation;
+    if (locale === 'american-to-british') {
+      translation = translator.translateToBritish(text);
+    } else if (locale === 'british-to-american') {
+      translation = translator.translateToAmerican(text);
+    } else {
+      return res.json({
+        error: 'Invalid value for locale field',
+      });
+    }
+
+    if (translation === text) {
+      return res.json({
+        text,
+        translation: 'Everything looks good to me!',
+      });
+    }
+
+    return res.json({ text, translation });
+  });
 };
