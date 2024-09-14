a29914f7c825d118815a2ac80c4e5b3769434c74
E-K8 <ekatplat@gmail.com>
2024-09-14 21:56:32 +0100

    functional test for translation with text and (in)valid locale field

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,5 +8,37 @@ chai.use(chaiHttp);
 let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
+  suite('POST /api/translate', () => {
+    test('Translation with text and locale fields', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+          text: 'Mangoes are my favorite fruit.',
+          locale: 'american-to-british',
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'text');
+          assert.property(res.body, 'translation');
+          done();
+        });
+    });
 
+    test('Translation with text and invalid locale field', function (done) {
+      chai
+        .request(server)
+        .post('/api/translate')
+        .send({
+          text: 'Mangoes are my favorite fruit.',
+          locale: 'invalid-locale',
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.property(res.body, 'error');
+          assert.equal(res.body.error, 'Invalid value for locale field');
+          done();
+        });
+    });
+  });
 });
